5681d0d5697d07bde02d9a9636c90f6993f90f0b
Richard <rahayn@yahoo.com>
2017-09-19 21:27:36 -0400

    Console App To Retrieve Network Ino
    Uses different methods to return out the ip address, hostname and mac
    address

---
+++ GetIP/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace GetMyNetworkInformation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string myMacAddress = GetMacAddress().ToString();
+            string ipv4Address = GetLocalIPAddress();
+            string computerName = GetLocalHostname();
+            Console.WriteLine("ComputerName:{0}\nPhysical Address:{1}\nIPV4Address:{2}",computerName,myMacAddress,ipv4Address);
+		}
+		public static string GetLocalHostname()
+		{
+			string myHostName = Dns.GetHostName();
+			return myHostName;
+		}
+
+        public static string GetLocalIPAddress()
+		{
+            string myIPAddress = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+            return myIPAddress;
+		}
+        public static PhysicalAddress GetMacAddress()
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Ethernet && nic.OperationalStatus == OperationalStatus.Up)
+                {
+                    return nic.GetPhysicalAddress();
+                }
+            }
+            return null;
+        }
+    }
+}
